6def75d88f4f05fc6bf8b6cca503c1e524533b32
Serhii Melnyk <serhiimelnik@gmail.com>
2015-02-22 22:46:03 +0200

    Part 7

--- src/app.php
+++ src/app.php
@@ -1,41 +1,15 @@
 <?php
 
 use Symfony\Component\Routing;
-require 'LeapYearController.php';
-
-function is_leap_year($year = null)
-{
-    if (null === $year) {
-        $year = date('Y');
-    }
-
-    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
-}
 
 $routes = new Routing\RouteCollection();
-$routes->add(
-    'hello',
-    new Routing\Route(
-        '/hello/{name}', array(
-            'name' => 'World',
-            '_controller' => 'render_template',
-        )
-    )
-);
-$routes->add(
-    'bye',
-    new Routing\Route(
-        '/bye', array(
-            '_controller' => 'render_template'
-        )
-    )
-);
+
 $routes->add(
     'leap_year',
     new Routing\Route(
         '/is_leap_year/{year}', array(
             'year' => null,
-            '_controller' => 'LeapYearController::indexAction',
+            '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
         )
     )
 );
--- web/front.php
+++ web/front.php
@@ -3,18 +3,8 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
-
-function render_template($request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -24,17 +14,7 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $resolver->getController($request);
-    $arguments = $resolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (Routing\Exception\RouteNotFoundException $e) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $e) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new \Simplex\Framework($matcher, $resolver);
+$response = $framework->handle($request);
 
 $response->send();
